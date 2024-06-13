e16879d7e4465db24a9c8833d2bba980183d3a4a
achyuth-roy <achyuth.pilly@scynaero.com>
2024-06-12 20:11:08 -0400

    formdata capture code fix

--- app/api/submitForm/route.js
+++ app/api/submitForm/route.js
@@ -1,10 +1,7 @@
-// app/api/submitForm/route.js
-
 import { NextResponse } from 'next/server';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
 import { v4 as uuidv4 } from 'uuid';
-
 
 // Function to get AWS credentials from GitHub Actions context
 function getAwsCredentials() {
@@ -19,50 +16,16 @@ function getAwsCredentials() {
   
   return { token, accessKeyId, secretAccessKey, region };
 }
-//////////////////////////
-const client = new DynamoDBClient({
-      region: process.env.AWS_REGION,
-      credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID ,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ,
-        sessionToken: process.env.AWS_SESSION_TOKEN
-      }
-    });
 
-
-const dynamoDb = DynamoDBDocumentClient.from(client);
-const TABLE_NAME = 'Contacts';
-
-async function addContact(contact){
-    const response = await dynamoDb.send(new PutCommand({
-    TableName: TABLE_NAME,
-    Item: contact
-    }))
-    console.log(response.Items)
-};
-
-const contact = {
-    id: uuidv4(), // Generate a unique ID for each entry
-    firstName: "lokesh",
-    lastName: "mahesh",
-    company: "thales",
-    email: "lokesh@gmail.com",
-    message: "test content.",
-    createdAt: new Date().toISOString(),
-        
-}
-///////////////////////////////////////
 export async function POST(req) {
   try {
     const { firstName, lastName, company, email, message } = await req.json();
-    const inputdata = {
-        id: uuidv4(), // Generate a unique ID for each entry
+    const formData = {
         firstName,
         lastName,
         company,
         email,
-        message,
-        createdAt: new Date().toISOString(),
+        message
     };
     
     // Retrieve AWS credentials from GitHub Actions context
@@ -80,6 +43,12 @@ export async function POST(req) {
     const dynamoDb = DynamoDBDocumentClient.from(client);
     const TABLE_NAME = 'Contacts'; // Replace with your DynamoDB table name
     
+    const inputdata = {
+        id: uuidv4(), // Generate a unique ID for each entry
+        ...formData,
+        createdAt: new Date().toISOString(),
+    };
+    
     const params = {
       TableName: TABLE_NAME,
       Item: inputdata
@@ -92,5 +61,3 @@ export async function POST(req) {
     return NextResponse.json({ error: 'Error saving data' }, { status: 500 });
   }
 }
-
-addContact(contact)
